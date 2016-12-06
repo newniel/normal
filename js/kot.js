41e2853207d1d0eb933284fb7b6d3d612c17790f
권오택 <oteak.kwon@titipi.com>
2016-12-06 20:42:39 +0900

    no message

--- js/kot.js
+++ js/kot.js
@@ -146,7 +146,7 @@ window.onload = function(){
 	canvas.height = H;
 	
 	//snowflake particles
-	var mp = 125; //max particles
+	var mp = 25; //max particles
 	var particles = [];
 	for(var i = 0; i < mp; i++)
 	{
