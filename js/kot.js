e2ffc835b23ba1927af445472c259384bef2d0ab
권오택 <oteak.kwon@titipi.com>
2016-12-06 20:16:30 +0900

    no message

--- js/kot.js
+++ js/kot.js
@@ -1,11 +1,3 @@
-$('.container').on('mouseover', function(){
-  $('.bauble').addClass('light');
-  $('.star').addClass('star-light');
-})
-
-$('.container').on('mouseout', function(){
-  $('.bauble').removeClass('light');
-})
 
 
 
